527338f3546e6ba1c67f6f290cc22f9e95aaf5cf
aldo sebastian <aldohasibuan1@gmail.com>
2020-06-29 23:04:16 +0200

    stuff

--- my_own_gui/my_own_gui.py
+++ my_own_gui/my_own_gui.py
@@ -25,14 +25,7 @@ import json
 import pandas as pd
 import numpy as np
 LARGE_FONT=("Verdana",12)
-NORM_FONT=("Verdana",10)
-SMALL_FONT=("Verdana",8)
 
-resampleSize="15Min"
-dataPace="tick"
-candleWidth=0.008
-paneCount=1
-chartLoad=1
 def changeTimeFrame(tf):
     global DataPace
     global DatCounter
@@ -71,8 +64,6 @@ def popupmsg(msg):
     popup.mainloop()
     
 def animate(i):
-    global refreshrate
-    global DatCounter
     
     #a=plt.subplot2grid((6,4),(0,0),rowspan=5,colspan=4)
     #a2=plt.subplot2grid((6,4),(0,0),rowspan=1,colspan=4,sharex=a)
@@ -100,7 +91,7 @@ def animate(i):
     a.plot_date(buyDates,buys["price"], "#00A3E0",label="buys")
     a.plot_date(sellDates,sells["price"],"#183A54",label="sells") #We use hex color in #
     
-    title="BTC-e BTCUSD Prices\nLast Price: {}".format(str(data['price'][len(data.columns)]))
+    title="BTC-e BTCUSD Price \nLast Price: {}".format(str(data['price'][len(data.columns)]))
     a.set_title(title)
     
     a.legend(bbox_to_anchor=(0,1.02,1,.102),loc=3,
@@ -141,50 +132,20 @@ class SeaofBTCapp(tk.Tk): #This class will inherit the attributes and methods of
         #create the "file" menubar
         menubar.add_cascade(label="File",menu=filemenu) 
         
-        currencyChoice=tk.Menu(menubar,tearoff=0)
-        currencyChoice.add_command(label="EUR",command=lambda:changeCurrency("EUR"))
-        currencyChoice.add_command(label="USD",command=lambda:changeCurrency("USD"))
-        currencyChoice.add_command(label="GBP",command=lambda:changeCurrency("GBP"))
-        menubar.add_cascade(label="currency",menu=currencyChoice)
-        
-        dataTF = tk.Menu(menubar,tearoff=0)
-        dataTF.add_command(label="Now",command=lambda:changeTimeFrame('tick'))
-        dataTF.add_command(label="1 Day",command=lambda:changeTimeFrame('1day'))
-        dataTF.add_command(label="3 Days",command=lambda:changeTimeFrame('3days'))
-        dataTF.add_command(label="1 Week",command=lambda:changeTimeFrame('1week'))
-        menubar.add_cascade(label="Data Time Frame",menu=dataTF)
-        
-        OHLCI=tk.Menu(menubar,tearoff=0)#open high low close interval
-        OHLCI.add_command(label="tick",command=lambda:changeTimeFrame('tick'))
-        OHLCI.add_command(label="1 minute",command=lambda:changeTimeFrame('1min',0.0005))
-        OHLCI.add_command(label="5 minute",command=lambda:changeTimeFrame('5min',0.003))
-        OHLCI.add_command(label="15 minute",command=lambda:changeTimeFrame('15min',0.008))
-        OHLCI.add_command(label="30 minute",command=lambda:changeTimeFrame('30min',0.016))
-        OHLCI.add_command(label="1 hour",command=lambda:changeTimeFrame('1H',0.032))
-        OHLCI.add_command(label="3 hour",command=lambda:changeTimeFrame('3H',0.096))
-        menubar.add_cascade(label="OHLCI",menu=OHLCI)
-
-
-        
-        
         tk.Tk.config(self,menu=menubar)
         
         self.frames={} #The frames will be inside a dictionary
         
-        for F in (StartPage,BTCE_page): #This initializes each frame; frame = page
-            self.frames[F]=F(container,self) #StartPage ... are classes defined below
+        for F in (StartPage,BTCE_page,dynamic_page): #This initializes each frame; frame = page
+            self.frames[F]=F(container,self) #y since F takes self as argument, each page can access any attributes and methods of SeaofBTTCapp
             self.frames[F].grid(row=0,column=0, sticky="nsew")
             
-        self.show_frame(StartPage)
-
-    def show_frame(self,cont):
-         frame=self.frames[cont] 
-         frame.tkraise()
+        self.frames[StartPage].tkraise()
         
 class StartPage(tk.Frame): #Each page inherits from the Frame class of tk package. It is called in the for loop
                            #in the SeaofBTC class above.
     
-    def __init__(self,parent,button): 
+    def __init__(self,parent,controller):  #controller is the "self" of SeaofBTCapp, which means we can access all the attributes and methods of SEaofBTCapp
         super().__init__(parent) #The parent argument here is container, which is processed by super()
         
         #This initializes the startpage texxt widget
@@ -192,38 +153,22 @@ class StartPage(tk.Frame): #Each page inherits from the Frame class of tk packag
                                       Use at your own risk!
                                       No warranty promise!""",bg="red",fg="white",font=LARGE_FONT)
         #passing self here since the label function takes in a frame object 
-        label.pack(fill=tk.X,pady=5) #tk.X fmakes 
+        label.pack(fill=tk.X)
         
         
         #This initializes the button in this window
         button1=ttk.Button(self,text="Agree",
-                          command=lambda: button.show_frame(BTCE_page))
+                          command=lambda: controller.frames[dynamic_page].tkraise())
         #lambda is used so that the function is not run on the first iteration
                                                     #of the loop
         button1.pack()
         
         #This initializes the button in this window
         button2=ttk.Button(self,text="Disagree",
-                          command=lambda: button.show_frame(StartPage))
+                          command=lambda: controller.frames[StartPage].tkraise())
         #lambda is used so that the function is not run on the first iteration
                                                     #of the loop
         button2.pack()
-        
-
-class PageOne(tk.Frame):
-    def __init__(self,parent,button):
-        super().__init__(parent)
-        
-        #This initializes the startpage window
-        label = ttk.Label(self,text="Page 1", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-        
-        #This initializes the button in this window
-        button1=ttk.Button(self,text="Back to start",
-                          command=lambda: button.show_frame(StartPage))
-        #lambda is used so that the function is not run on the first iteration
-                                                    #of the loop
-
 
 class BTCE_page(tk.Frame):
     def __init__(self,parent,button):
@@ -235,12 +180,38 @@ class BTCE_page(tk.Frame):
         
         #This initializes the button in this window
         button1=ttk.Button(self,text="Back to start",
-                          command=lambda: button.show_frame(StartPage))
+                          command=lambda: controller.frames[StartPage].tkraise())
         #lambda is used so that the function is not run on the first iteration
                                                     #of the loop
         button1.pack()
         
-
+        #a.plot([range(1,8),range(3,10)]) #plot sample data on the given subplot
+        
+        #we would usually do a.show(), but here it will not plot on our gui
+        
+        canvas=FigureCanvasTkAgg(f,self) #with self, we send in our gui page as
+        canvas.draw() #draw figure on canvas
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True) #pack the canvas on tkinter
+        
+        toolbar=NavigationToolbar2Tk(canvas,self) #with self, we send the 
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        
+class dynamic_page(tk.Frame):
+    def __init__(self,parent,button):
+        super().__init__(parent)
+        
+        #This initializes the startpage window
+        label = ttk.Label(self,text="Graph Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+        
+        #This initializes the button in this window
+        button1=ttk.Button(self,text="Back to start",
+                          command=lambda: controller.frames[StartPage].tkraise())
+        #lambda is used so that the function is not run on the first iteration
+                                                    #of the loop
+        button1.pack()
+        
         #a.plot([range(1,8),range(3,10)]) #plot sample data on the given subplot
         
         #we would usually do a.show(), but here it will not plot on our gui
--- video8/parse_json_api_example.py
+++ video8/parse_json_api_example.py
@@ -1,17 +1,31 @@
 import urllib.request
 import json
+import numpy as np
+import pandas as pd
 import time
 
 while True:
-    dataLink="https://bitbay.net/API/Public/BTC/all.json"
+    #dataLink="https://bitbay.net/API/Public/BTC/trades.json"
+    dataLink="https://api.coindesk.com/v1/bpi/historical/close.json"
     data=urllib.request.urlopen(dataLink)
     data=data.read()
     data=json.loads(data)
+    data=pd.DataFrame(data) #pandas DataFrame will convert
+                            #the keys into heading of a table and
+                            #the values of each key as the rows
+    #data['time']=np.array(data['time']).astype("datetime64[s]")
+
+
+#     buys=data[data['type']=="buy"]
+#     buys['date']=np.array(buys['date']).astype("datetime64[s]")
+#     buyDates=(buys['date']).tolist()
+#     
+#     sells=data[data['type']=="buy"]
+#     sells['date']=np.array(buys['date']).astype("datetime64[s]")
+#     sellDates=(sells['date']).tolist()
+
+    print(data['time'])
     
-    trans=data['transactions'][0]
-    print(data['transactions'][0]['date'])
-    
-    print(data)
     time.sleep(2)
     
     
